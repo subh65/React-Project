b42e7e5ab332c1d89523f570d0ecf2ffd313c02e
subham singh <122512576+subh65@users.noreply.github.com>
2025-01-28 15:49:25 +0530

    Add files via upload

---
+++ QuizApp/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import Quiz from './Quiz'
+import './QuizApp.css'
+
+
+function App() {
+  return (
+    <>
+     <Quiz/>
+    </>
+  )
+}
+
+export default App
